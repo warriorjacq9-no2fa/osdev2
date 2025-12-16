a4833dac2b27967981c521bfd4abac133e8793b7
warriorjacq9-no2fa <jflusche3180@student.pps.net>
2025-12-16 21:24:10 +0000

    Add A20 line init to bootstrap

--- kernel/arch/i386/bootstrap.asm
+++ kernel/arch/i386/bootstrap.asm
@@ -29,8 +29,48 @@ read_loop:
     int 0x13
     jc disk_error
     cli
+    mov ax, 0x7F00
+    mov sp, ax
     ; now kernel is at 0x10000
 
+    ; Enable the A20 line
+    call    a20wait
+    mov     al,0xAD
+    out     0x64,al
+
+    call    a20wait
+    mov     al,0xD0
+    out     0x64,al
+
+    call    a20wait2
+    in      al,0x60
+    push    eax
+
+    call    a20wait
+    mov     al,0xD1
+    out     0x64,al
+
+    call    a20wait
+    pop     eax
+    or      al,2
+    out     0x60,al
+
+    call    a20wait
+    mov     al,0xAE
+    out     0x64,al
+    jmp done
+
+a20wait:
+    in      al,0x64
+    test    al,2
+    jnz     a20wait
+    ret
+a20wait2:
+    in      al,0x64
+    test    al,1
+    jz      a20wait2
+    ret
+done:
     ;==========================
     ; Setup GDT
     ;==========================
